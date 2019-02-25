ae7443b7bebe9adfdd6497e0fc39fcfa68659452
Fikhri <36291552+fikhrimasri@users.noreply.github.com>
2019-02-25 22:52:04 +0700

    ubahsandi.php
    ubah sandi

--- controller/ubahsandi.php
+++ controller/ubahsandi.php
@@ -1,1 +1,23 @@
+<?php
+require_once("connect.php");
 
+if(isset($_POST['lupasandi'])){
+  $passwordl = $_POST['sandilama'];
+  $passwordb = $_POST['sandibaru'];
+  $sql = "UPDATE Akun SET password=:passwordb WHERE password=:passwordl";
+  $stmt = $db->prepare($sql);
+  // bind parameter ke query
+  $params = array(
+      ":passwordl" => $passwordl,
+      ":passwordb" => $passwordb
+  );
+  $stmt->execute($params);
+
+  if($stmt){
+    header("Location: login.php??ubah-sandi-sukses");
+  }else{
+    header("Location: #");
+  }
+}
+
+?>
